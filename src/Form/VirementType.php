8e5eecab1db652b993c2e2a567594657d70a4f25
dcaramello <dcaramello@gmail.com>
2020-11-16 13:41:12 +0100

    changes the amounts of the accounts with the transfer

---
+++ src/Form/VirementType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class VirementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $accounts = $options['accounts'];
+        $choices = [];
+        foreach ($accounts as $account){
+            $choices[] = $account->getId();
+        }
+
+        $builder
+            ->add('debit', ChoiceType::class, [
+                'label' => 'Compte à débiter',
+                'choices' => $choices,
+            ])
+            ->add('amount', TextType::class, [
+                'label' => 'Montant',
+            ])
+            ->add('credit', ChoiceType::class, [
+                'label' => 'Compte à créditer',
+                'choices' => $choices,
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    "class" => "btn btn-danger"
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+            // 'data_class' => Account::class,
+        ]);
+        $resolver->setRequired('accounts');
+    }
+}
